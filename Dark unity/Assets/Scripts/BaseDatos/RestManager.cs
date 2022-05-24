a07028248a99ffb2fe2befa462e9f2e16d14f8f7
Abel <abelanayapardo85@gmail.com>
2022-05-25 01:26:06 +0200

    Conexión con la API realizada, ranking hecho y funcional.

---
+++ Dark unity/Assets/Scripts/BaseDatos/RestManager.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+using SimpleJSON;
+//using Json.Net;
+
+public class RestManager : MonoBehaviour
+{
+    public static RestManager instance;
+
+    public GameObject ranking;
+
+    public Text infoText;
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(GetRanking());
+    }
+
+    
+
+    public IEnumerator GetRanking()
+    {
+        //infoText.text = "Cargando datos...";
+        UnityWebRequest www = UnityWebRequest.Get("http://192.168.136.1:8080/usuarios");
+        
+        yield return www.SendWebRequest();
+        
+
+        if(www.isNetworkError || www.isHttpError)
+        {
+            Debug.Log(www.error);
+        }
+        else
+        {
+            Debug.Log(www.downloadHandler.text); //Muestra el resulta como texto
+
+            JSONNode data = JSON.Parse(www.downloadHandler.text);
+
+            JSONNode id = data[0];
+            
+            foreach(JSONNode puntuacion in data)
+            {
+                
+                
+                if(id["puntos"] < puntuacion["puntos"])
+                {
+                    id = puntuacion;
+
+                }
+
+                infoText.text += "Nombre: " +puntuacion["nombreUsuario"] +"/";
+                infoText.text += "Puntos: " +puntuacion["puntos"] +"\n" +"\n" +"\n";
+            }
+
+            infoText.text += "El usuario con mas puntos es " +id["nombreUsuario"] +" con " + id["puntos"];
+        }
+
+    }
+}
